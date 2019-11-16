e9dad8bb8768e4347c65b2e573eebd595d14a30a
Youssef Alshaarawi <youssefalshaarawi@yahoo.com>
2019-11-16 13:06:26 +0200

    Gui working

--- src/eg/edu/alexu/csd/oop/cs71/db/Gui.java
+++ src/eg/edu/alexu/csd/oop/cs71/db/Gui.java
@@ -1,7 +1,14 @@
 package eg.edu.alexu.csd.oop.cs71.db;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+
 
 public class Gui extends Application {
 
@@ -9,9 +16,54 @@ public class Gui extends Application {
         launch(args);
     }
 
+    private TableView table = new TableView();
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) {
+        Scene scene = new Scene(new Group());
+        stage.setTitle("Table View Sample");
+        stage.setWidth(300);
+        stage.setHeight(500);
 
+        TextField textField =new TextField();
+        Button button =new Button("Run");
+        Parser parser=new Parser();
+        button.setOnAction(e->{
+            String query=textField.getText();
+            query = query.replaceAll("( )+", " ");
+            if(parser.validateQuery(query))
+            {
+                parser.parse(query);
+            }
+            else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Error");
+                alert.setHeaderText("Invalid query");
+                alert.setContentText("Please try again!");
+                alert.showAndWait();
+            }
+        });
+
+
+        table.setEditable(true);
+
+        //-LOOP ON TABLE COLUMNS-
+        //https://docs.oracle.com/javafx/2/ui_controls/table-view.htm
+
+        /*TableColumn firstNameCol = new TableColumn("First Name");
+        TableColumn lastNameCol = new TableColumn("Last Name");
+        TableColumn emailCol = new TableColumn("Email");*/
+
+       // table.getColumns().addAll(firstNameCol, lastNameCol, emailCol);
+
+        final VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(10, 0, 0, 10));
+        vbox.getChildren().addAll(textField,button, table);
+
+        ((Group) scene.getRoot()).getChildren().addAll(vbox);
+
+        stage.setScene(scene);
+        stage.show();
 
     }
 }
---
+++ src/eg/edu/alexu/csd/oop/cs71/db/Parser.java
@@ -0,0 +1,13 @@
+package eg.edu.alexu.csd.oop.cs71.db;
+
+public class Parser {
+
+    public boolean validateQuery(String q)
+    {
+        return false;
+    }
+
+    public void parse(String query) {
+
+    }
+}
