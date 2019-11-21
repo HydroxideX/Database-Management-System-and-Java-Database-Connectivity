7afed33fd38e8d4619e6c71fc05d283e14df796a
omarshawky15 <47141484+omarshawky15@users.noreply.github.com>
2019-11-22 00:14:45 +0200

    Update Edit.java

--- src/eg/edu/alexu/csd/oop/cs71/db/Edit.java
+++ src/eg/edu/alexu/csd/oop/cs71/db/Edit.java
@@ -12,19 +12,22 @@ public class Edit {
         for(int i=0 ;i<operation.length;i++){String s = operation[i].toLowerCase();
             if(s.equals("and")||s.equals("or")||s.equals("not")) {
                 logicOperator.add(s);
-                System.out.print(s+ " ");
+                //System.out.print(s+ " ");
             }
         }
     }
     private void operationParser (String query) {
         query = query .replaceAll("\\s+|\\(+|\\)|\\,|\\'|\\\"|\\;"," ");
+        query = query .replaceAll("\\!\\s*(?!\\=)","not ");
+        query = query .replaceAll("(\\&\\s*\\&) ","and");
+        query = query .replaceAll("(\\|\\s*\\|) ","or");
         logicOperatoParser(query);
         query = query .replaceAll("(?i)(not)\\s*","");
         query = query .replaceAll("\\s+"," ");
-        System.out.println(query);
+        //System.out.println(query);
         String [] operation = query.split("(?i)(\\s*(and|or)\\s*)");
         for(int i=0 ;i<operation.length; i++){
-            String [] parameters = operation[i].split(" ");
+            String [] parameters = operation[i].split("\\s+");
             String cN =parameters[0] , oN = parameters[1];
             columnNames.add(cN);operationNames.add(oN.toLowerCase());
             ArrayList<String> temp = new ArrayList<String>();
@@ -33,11 +36,11 @@ public class Edit {
            }
            oPParameters.add(temp);
         }
-        for(int i=0 ;i<columnNames.size();i++){
+       /* for(int i=0 ;i<columnNames.size();i++){
             System.out.print(columnNames.get(i) + " " + operationNames.get(i) + " ");
             for(int j =0 ;j<oPParameters.get(i).size();j++)System.out.print(oPParameters.get(i).get(j)+" ");
             System.out.println();
-        }
+        }*/
     }
     public void operationPerformer (ArrayList<HashMap<String,Object>>table ){
         for(int i=0 ;i<table.size() ; i++){
@@ -68,9 +71,29 @@ public class Edit {
                     case "=" :
                         if(t.get(columnName).toString().equals(pp.get(0)))ans+="1";
                         else ans+="0";
-                }
+                        break;
+                    case ">=" :
+                        if(t.get(columnName).toString().compareTo(pp.get(0)) >= 0)ans+="1";
+                        else ans+= "0";
+                        break;
+                    case "<=" :
+                        if(t.get(columnName).toString().compareTo(pp.get(0)) <= 0)ans+="1";
+                        else ans+= "0";
+                        break;
+                    case ">" :
+                        if(t.get(columnName).toString().compareTo(pp.get(0)) > 0)ans+="1";
+                        else ans+= "0";
+                        break;
+                    case "<" :
+                        if(t.get(columnName).toString().compareTo(pp.get(0)) < 0)ans+="1";
+                        else ans+= "0";
+                        break;
+                    case "!=" : case "<>" :
+                        if(t.get(columnName).toString().compareTo(pp.get(0)) != 0)ans+="1";
+                        else ans+= "0";
+                        break;                }
         }
-            System.out.println(ans);
+            //System.out.println(ans);
             answers.add(ans);
         }
     }
@@ -83,7 +106,7 @@ public class Edit {
             if(s.equals("not"))a=!a;
             else {
                 b= (ans.charAt(j++)=='1')?true:false;
-                while(i+1<logicOperator.size()&&logicOperator.get(i+1).toLowerCase().equals("not")){b=!b;i++;}
+                while(i+1<logicOperator.size()&&(logicOperator.get(i+1).toLowerCase().equals("not"))){b=!b;i++;}
                 switch(s){
                     case "and":
                         a&=b;
@@ -94,24 +117,53 @@ public class Edit {
                 }
             }
                     }
-        System.out.println(a);
+        //System.out.println(a);
         return a ;
     }
-    public void update (ArrayList<HashMap<String,Object>>table ,String query ){
+    public void update (ArrayList<HashMap<String,Object>>table ,String query ) {
         int sz = table.size();
-        query = "UPDATE table_name SET subject = value1, age = value2 WHERE not subject IN ('Maths', 'Science') and not not age between ('15', '18') or not name = 'compu';";
         query = query.split("(\\w+\\s*)+(?i)(set)\\s*")[1];
         String ch = query.split("\\s*(?i)(where)\\s*")[0];
-        String []changes = ch.split("(\\s*\\=\\s*)|(\\s*\\,\\s*)");
-        query = query.split("\\s*(?i)(where)\\s*")[1];
-        operationParser(query);
-        operationPerformer(table);
-        for(int i=0 ;i<table.size() ;i++){
-            if(isTrue(answers.get(i))){
-                HashMap<String,Object> row = table.get(i);
-                for(int j=0 ;j<changes.length;j+=2)row.put(changes[j],changes[j+1]);
+        String[] changes = ch.split("(\\s*\\=\\s*)|(\\s*\\,\\s*)");
+        if (query.toLowerCase().contains("where")) {
+            query = query.split("\\s*(?i)(where)\\s*")[1];
+            operationParser(query);
+            operationPerformer(table);
+            for (int i = 0; i < table.size(); i++) {
+                if (isTrue(answers.get(i))) {
+                    HashMap<String, Object> row = table.get(i);
+                    for (int j = 0; j < changes.length; j += 2) row.put(changes[j], changes[j + 1]);
+                }
+            }
+            logicOperator.clear();
+            operationNames.clear();
+            answers.clear();
+            columnNames.clear();
+            oPParameters.clear();
+        } else
+            for (int i = 0; i < table.size(); i++) {
+                HashMap<String, Object> row = table.get(i);
+                for (int j = 0; j < changes.length; j += 2) row.put(changes[j], changes[j + 1]);
+            }
+    }
+    public void delete (ArrayList<HashMap<String,Object>>table ,String query ){
+        if(query.toLowerCase().contains("where")) {
+            query = query.split("\\s*(?i)(where)\\s*")[1];
+            operationParser(query);
+            operationPerformer(table);
+            for (int i = 0; i < table.size(); i++) {
+                if (isTrue(answers.get(i))) {
+                    answers.remove(i);
+                    table.remove(i);
+                    i--;
+                }
             }
         }
+        else
+            for (int i = 0; i < table.size(); i++) {
+                table.remove(i);
+                i--;
+            }
     }
     public static void  main(String[] args){
         Edit e = new Edit();
@@ -126,12 +178,31 @@ public class Edit {
         row.put("age","19");
         row.put("name","omar");
         ex.add(row);
+        System.out.println("\nBefore Update_1");
         for(int i=0 ;i<ex.size(); i++){
             System.out.println(ex.get(i).get("name").toString()+ " "+ex.get(i).get("age").toString()+" "+ex.get(i).get("subject").toString());
         }
-        e.update(ex,"UPDATE table_name SET column1 = value1, column2 = value2 WHERE not subject IN ('Maths', 'Science') and not not age between ('15', '18') or not name = 'compu';");
+        e.update(ex,"UPDATE table_name SET subject = value1, age = value2 WHERE not subject IN ('Maths', 'Science') and not not age between ('15', '18') or not name = 'compu';");
+        System.out.println("\nAfter Update_1");
+
         for(int i=0 ;i<ex.size(); i++){
-           System.out.println(ex.get(i).get("name").toString()+ " "+ex.get(i).get("age").toString()+" "+ex.get(i).get("subject").toString());
+            System.out.println(ex.get(i).get("name").toString()+ " "+ex.get(i).get("age").toString()+" "+ex.get(i).get("subject").toString());
+        }
+        System.out.println("\nBefore Update_2");
+        for(int i=0 ;i<ex.size(); i++){
+            System.out.println(ex.get(i).get("name").toString()+ " "+ex.get(i).get("age").toString()+" "+ex.get(i).get("subject").toString());
+        }
+        e.update(ex,"UPDATE table_name SET subject = value1, age = value2 ");
+        System.out.println("\nAfter Update_2");
+
+        for(int i=0 ;i<ex.size(); i++){
+            System.out.println(ex.get(i).get("name").toString()+ " "+ex.get(i).get("age").toString()+" "+ex.get(i).get("subject").toString());
+        }
+        e.delete(ex,"DELETE From table_name WHERE not subject IN ('Maths', 'Science') and not not age between ('15', '18') or not name = 'compu'; ");
+
+        System.out.println(" \nAfter Delete");
+        for(int i=0 ;i<ex.size(); i++){
+            System.out.println(ex.get(i).get("name").toString()+ " "+ex.get(i).get("age").toString()+" "+ex.get(i).get("subject").toString());
         }
     }
 }
