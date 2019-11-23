40a83b5f1462828a3bc35f566ae610bdfd79e43d
omarshawky15 <47141484+omarshawky15@users.noreply.github.com>
2019-11-23 15:58:22 +0200

    Update Parser.java

--- src/eg/edu/alexu/csd/oop/cs71/db/Parser.java
+++ src/eg/edu/alexu/csd/oop/cs71/db/Parser.java
@@ -1,15 +1,18 @@
 package eg.edu.alexu.csd.oop.cs71.db;
 
+import jdk.nashorn.internal.codegen.CompilationException;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Parser {
 
-    ArrayList <String> columnNames= new ArrayList<>();
-    ArrayList <String> operationNames = new ArrayList<>();
-    ArrayList <String>  logicOperator =  new ArrayList<>();
-    ArrayList <String> answers = new ArrayList<>();
-    ArrayList <ArrayList<String >> oPParameters = new ArrayList<>();
+    private ArrayList<String> columnNames = new ArrayList<>();
+    private ArrayList<String> operationNames = new ArrayList<>();
+    private ArrayList<String> logicOperator = new ArrayList<>();
+    private ArrayList<String> answers = new ArrayList<>();
+    private ArrayList<ArrayList<String>> oPParameters = new ArrayList<>();
 
     /*
     private String[] keywordArrayEquals = {"SELECT","FROM","*","WHERE","AND","OR","NOT", "'","ORDER", "BY",
@@ -45,7 +48,7 @@ public class Parser {
         return false;
     }*/
 
-    public ArrayList<ArrayList<String>> select(String query, ArrayList<String > colNames, ArrayList<String> colTypes, ArrayList<HashMap<String, Object>> table) {
+    public ArrayList<ArrayList<String>> select(String query, ArrayList<String> colNames, ArrayList<String> colTypes, ArrayList<HashMap<String, Object>> table) {
         logicOperator.clear();
         operationNames.clear();
         answers.clear();
@@ -54,8 +57,8 @@ public class Parser {
         ArrayList<String> printColumns = new ArrayList<>();
         ArrayList<String> selectedRows = new ArrayList<>();
         ArrayList<String> orderColumns = new ArrayList<>();
-        query.replaceAll("<>","!=");
-        String q = query.substring(7,query.length());
+        query.replaceAll("<>", "!=");
+        String q = query.substring(7, query.length());
         String[] collection = q.split(" ");
         ArrayList<String> ans = new ArrayList<String>();
         int i = 0;
@@ -74,19 +77,19 @@ public class Parser {
                 }
                 i++;
             }
-            i+=2;
+            i += 2;
         }
-        if (i <collection.length && collection[i].toUpperCase().contains("WHERE")) {
+        if (i < collection.length && collection[i].toUpperCase().contains("WHERE")) {
             ans.add(collection[i++]);
             String s = "";
-            while(i < collection.length && !collection[i].toUpperCase().contains("ORDER")){
-                s = s + " " +collection[i];
+            while (i < collection.length && !collection[i].toUpperCase().contains("ORDER")) {
+                s = s + " " + collection[i];
                 i++;
             }
-            operationParser(s.substring(1,s.length()),colNames,colTypes,table);
-            operationPerformer(colNames,colTypes,table);
+            operationParser(s.substring(1, s.length()), colNames, colTypes, table);
+            operationPerformer(colNames, colTypes, table);
             for (int row = 0; row < table.size(); row++) {
-                if (isTrue(answers.get(row),colNames,colTypes,table)) {
+                if (isTrue(answers.get(row), colNames, colTypes, table)) {
                     selectedRows.add(String.valueOf(row));
                 }
             }
@@ -94,9 +97,9 @@ public class Parser {
             selectedRows.add("-1");
         }
 
-        if(i<collection.length && collection[i].toUpperCase().contains("ORDER")){
-            i+=2;
-            while (i<collection.length) {
+        if (i < collection.length && collection[i].toUpperCase().contains("ORDER")) {
+            i += 2;
+            while (i < collection.length) {
                 if (collection[i].charAt(collection[i].length() - 1) == ',') {
                     orderColumns.add(collection[i].substring(0, collection[i].length() - 1));
                 } else {
@@ -115,94 +118,136 @@ public class Parser {
         return result;
     }
 
-    public void update (String query, ArrayList<String > colNames, ArrayList<String> colTypes, ArrayList<HashMap<String, Object>> table ) {
+
+    public int update(String query, ArrayList<HashMap<String, Object>> table ,ArrayList<String>XDS_1, ArrayList<String> XDS_2) {
         logicOperator.clear();
         operationNames.clear();
         answers.clear();
         columnNames.clear();
         oPParameters.clear();
-        int sz = table.size();
+        int counter = 0;
         query = query.split("(\\w+\\s*)+(?i)(set)\\s*")[1];
         String ch = query.split("\\s*(?i)(where)\\s*")[0];
         String[] changes = ch.split("(\\s*\\=\\s*)|(\\s*\\,\\s*)");
+      ArrayList<String> chColumn = new ArrayList<String>();
+        ArrayList<ArrayList<String>> chValue = new ArrayList<ArrayList<String>>();
+        for (int i = 0; i < changes.length; i += 2) {
+            ArrayList<String>temp  = new ArrayList<String>();
+            chColumn.add(changes[i]);
+            temp.add(changes[i+1]);
+            chValue.add(temp);
+            System.out.println(changes[i] + " " + changes[i + 1]);
+        }
+        Validation_Tany_3shan_5ater_sh3rawy_2lvalidation_bta3th_mbt3mlsh_7aga(chColumn,chValue,XDS_1,XDS_2);
         if (query.toLowerCase().contains("where")) {
             query = query.split("\\s*(?i)(where)\\s*")[1];
-            operationParser(query,colNames,colTypes,table);
-            operationPerformer(colNames,colTypes,table);
+            operationParser(query, XDS_1, XDS_2, table);
+            Validation_Tany_3shan_5ater_sh3rawy_2lvalidation_bta3th_mbt3mlsh_7aga(columnNames,oPParameters,XDS_1,XDS_2);
+            operationPerformer(XDS_1, XDS_2, table);
             for (int i = 0; i < table.size(); i++) {
-                if (isTrue(answers.get(i),colNames,colTypes,table)) {
+                if (isTrue(answers.get(i), XDS_1, XDS_2, table)) {
                     HashMap<String, Object> row = table.get(i);
-                    for (int j = 0; j < changes.length; j += 2) row.put(changes[j], changes[j + 1].replaceAll("\'",""));
+                    for (int j = 0; j < chColumn.size(); j ++) row.put(chColumn.get(j), chValue.get(j).get(0));
+                    counter++;
+
                 }
             }
         } else
             for (int i = 0; i < table.size(); i++) {
                 HashMap<String, Object> row = table.get(i);
-                for (int j = 0; j < changes.length; j += 2) row.put(changes[j], changes[j + 1]);
+                for (int j = 0; j < chColumn.size(); j ++) row.put(chColumn.get(j), chValue.get(j).get(0));
+                counter++;
+
             }
+        return counter;
     }
 
-    public void delete (String query, ArrayList<String > colNames, ArrayList<String> colTypes, ArrayList<HashMap<String, Object>> table ){
+    public int delete(String query,  ArrayList<HashMap<String, Object>> table,ArrayList<String> colNames, ArrayList<String> colTypes) {
         logicOperator.clear();
         operationNames.clear();
         answers.clear();
         columnNames.clear();
         oPParameters.clear();
-        if(query.toLowerCase().contains("where")) {
+        int counter = 0;
+        if (query.toLowerCase().contains("where")) {
             query = query.split("\\s*(?i)(where)\\s*")[1];
-            operationParser(query,colNames,colTypes,table);
-            operationPerformer(colNames,colTypes,table);
+            operationParser(query, colNames, colTypes, table);
+            operationPerformer(colNames, colTypes, table);
             for (int i = 0; i < table.size(); i++) {
-                if (isTrue(answers.get(i),colNames,colTypes,table)) {
+                if (isTrue(answers.get(i), colNames, colTypes, table)) {
                     answers.remove(i);
                     table.remove(i);
+                    counter++;
                     i--;
                 }
             }
-        }
-        else
+        } else
             for (int i = 0; i < table.size(); i++) {
                 table.remove(i);
+                counter++;
                 i--;
             }
+        return counter;
     }
 
-    private  void logicOperatorParser (String query, ArrayList<String > colNames, ArrayList<String> colTypes, ArrayList<HashMap<String, Object>> table){
-        String [] operation = query.split(" ");
-        for(int i=0 ;i<operation.length;i++){String s = operation[i].toLowerCase();
-            if(s.equals("and")||s.equals("or")||s.equals("not")) {
+    private void logicOperatorParser(String query, ArrayList<String> colNames, ArrayList<String> colTypes, ArrayList<HashMap<String, Object>> table) {
+        String[] operation = query.split(" ");
+        for (int i = 0; i < operation.length; i++) {
+            String s = operation[i].toLowerCase();
+            if (s.equals("and") || s.equals("or") || s.equals("not")) {
                 logicOperator.add(s);
             }
         }
     }
-    private void operationParser (String query, ArrayList<String > colNames, ArrayList<String> colTypes, ArrayList<HashMap<String, Object>> table) {
-        query = query .replaceAll("\\s+|\\(+|\\)|\\,|\\'|\\\"|\\;"," ");
-        query = query .replaceAll("\\!\\s*(?!\\=)","not ");
-        query = query .replaceAll("(\\&\\s*\\&) ","and");
-        query = query .replaceAll("(\\|\\s*\\|) ","or");
-        logicOperatorParser(query,colNames,colTypes,table);
-        query = query .replaceAll("(?i)(not)\\s*","");
-        query = query .replaceAll("\\s+"," ");
-        String [] operation = query.split("(?i)(\\s*(and|or)\\s*)");
-        for (int i=0 ;i<operation.length; i++) {
-            String [] parameters = operation[i].split("\\s+");
-            String cN =parameters[0] , oN = parameters[1];
-            columnNames.add(cN);operationNames.add(oN.toLowerCase());
+
+    private void operationParser(String query, ArrayList<String> colNames, ArrayList<String> colTypes, ArrayList<HashMap<String, Object>> table) {
+        query = query.replaceAll("\\s+|\\(+|\\)|\\,|\\;", " ");
+        logicOperatorParser(query, colNames, colTypes, table);
+        query = query.replaceAll("(?i)(not)\\s*", "");
+        String[] operation = query.split("(?i)(\\s*(and|or)\\s*)");
+        for (int i = 0; i < operation.length; i++) {
+            String[] parameters = operation[i].split("\\s+");
+            String cN = parameters[0], oN = parameters[1];
+            columnNames.add(cN);
+            operationNames.add(oN.toLowerCase());
             ArrayList<String> temp = new ArrayList<String>();
-            for (int j=2;j<parameters.length;j++) {
-                if(!parameters[j].equals(""))temp.add(parameters[j]);
+            for (int j = 2; j < parameters.length; j++) {
+                if (!parameters[j].equals("")) temp.add(parameters[j]);
             }
             oPParameters.add(temp);
+
+        }
+        /*for(int i=0 ;i<columnNames.size();i++){
+            System.out.print(columnNames.get(i) + " " + operationNames.get(i) + " ");
+            for(int j =0 ;j<oPParameters.get(i).size();j++)System.out.print(oPParameters.get(i).get(j)+" ");
+            System.out.println();
+        }*/
+    }
+    public void Validation_Tany_3shan_5ater_sh3rawy_2lvalidation_bta3th_mbt3mlsh_7aga(ArrayList<String> Column, ArrayList<ArrayList<String>> Value, ArrayList<String> XDS_1, ArrayList<String> XDS_2) {
+        for (int i = 0; i < Column.size(); i++) {
+            int idx = XDS_1.indexOf(Column.get(i));
+            if (idx == -1) throw new NullPointerException("Column Name Doesn't Exist ,WTF ?! ");
+            for (int j=0 ;j<Value.get(i).size() ; j++) {
+                System.out.println(i + " " +j);
+                System.out.println(Column.get(i) + " " +Value.get(i).get(j));
+                if (XDS_2.get(idx).toLowerCase().equals("int") && Value.get(i).get(j).contains("'"))
+                    throw new RuntimeException("Query is Wrong You Fool" + Column.get(i) + "isn't an Int");
+                if (XDS_2.get(idx).toLowerCase().equals("varchar") && !Value.get(i).get(j).contains("'"))
+                    throw new RuntimeException("Query is Wrong You Stubid , in " + Column.get(i) + " column value '' is missing ");
+                Value.get(i).set(j, Value.get(i).get(j).replaceAll("\\'", ""));
+
+            }
         }
     }
-    public void operationPerformer (ArrayList<String > colNames, ArrayList<String> colTypes, ArrayList<HashMap<String, Object>> table) {
+
+    public void operationPerformer(ArrayList<String> colNames, ArrayList<String> colTypes, ArrayList<HashMap<String, Object>> table) {
         for (int i = 0; i < table.size(); i++) {
             String ans = "";
             HashMap<String, Object> t = table.get(i);
             for (int j = 0; j < operationNames.size(); j++) {
                 ArrayList<String> pp = oPParameters.get(j);
                 String columnName = columnNames.get(j);
-                String type = getColumnType(columnName,colNames,colTypes);
+                String type = getColumnType(columnName, colNames, colTypes);
                 if (type.equals("varchar")) {
                     switch (operationNames.get(j)) {
                         case "between":
@@ -301,70 +346,69 @@ public class Parser {
         }
     }
 
-    String getColumnType(String columnName,ArrayList<String > colNames, ArrayList<String> colTypes){
-        for(int i = 0;i <colNames.size();i++){
-            if(columnName.equals(colNames.get(i))) return colTypes.get(i);
+    String getColumnType(String columnName, ArrayList<String> colNames, ArrayList<String> colTypes) {
+        for (int i = 0; i < colNames.size(); i++) {
+            if (columnName.equals(colNames.get(i))) return colTypes.get(i);
         }
         return "";
     }
 
-    public Boolean isTrue (String ans, ArrayList<String > colNames, ArrayList<String> colTypes, ArrayList<HashMap<String, Object>> table) {
-        String a = "",b = "" ;
+    public Boolean isTrue(String ans, ArrayList<String> colNames, ArrayList<String> colTypes, ArrayList<HashMap<String, Object>> table) {
+        String a = "", b = "";
         boolean negative;
-        int j = 0 ;
-        String callable ="";
-        for (int i=0 ;i<logicOperator.size(); i++){
+        int j = 0;
+        String callable = "";
+        for (int i = 0; i < logicOperator.size(); i++) {
             negative = false;
             String s = logicOperator.get(i);
-            if (s.equals("not"))
-            {
+            if (s.equals("not")) {
                 negative = !negative;
                 i++;
-                if(i != logicOperator.size()) s = logicOperator.get(i);
+                if (i != logicOperator.size()) s = logicOperator.get(i);
             }
-            if(ans.charAt(j)=='1') a="TRUE";
+            if (ans.charAt(j) == '1') a = "TRUE";
             else a = "FALSE";
             j++;
-            if(negative) {
-                if(a == "TRUE") {
+            if (negative) {
+                if (a == "TRUE") {
                     a = "FALSE";
                 } else {
                     a = "TRUE";
                 }
             }
             callable = callable + a;
-            switch(s) {
+            switch (s) {
                 case "and":
                     callable = callable + " AND ";
                     break;
-                case "or" :
+                case "or":
                     callable = callable + " OR ";
                     break;
             }
         }
-        if(j <ans.length()) {
+        if (j < ans.length()) {
             if (ans.charAt(j) == '1') callable += "TRUE";
             else callable += "FALSE";
         }
         return parseTrueAndFalse(callable);
     }
 
-    public boolean parseTrueAndFalse(String expression){
+    public boolean parseTrueAndFalse(String expression) {
         String[] stringsList = expression.split(" ");
         ArrayList<String> arrayListOfExpression = new ArrayList<>();
-        for(int i = 0;i< stringsList.length;i++){
+        for (int i = 0; i < stringsList.length; i++) {
             arrayListOfExpression.add(stringsList[i]);
         }
         arrayListOfExpression = parseAnd(arrayListOfExpression);
         return parseOr(arrayListOfExpression);
     }
 
-    private ArrayList<String> parseAnd(ArrayList<String> strings){
-        ArrayList<String> answerList  = new ArrayList<String>();
+    private ArrayList<String> parseAnd(ArrayList<String> strings) {
+        ArrayList<String> answerList = new ArrayList<String>();
         int temp = -1;
-        for(int i = 0;i<strings.size();i++) {
-            if(strings.get(i).equals("AND")) {
-                if(answerList.get(temp).equals("TRUE") && strings.get(i+1).equals("TRUE")) {
+        for (int i = 0; i < strings.size(); i++) {
+            if (strings.get(i).equals("AND")) {
+                if (answerList.get(temp).equals("TRUE") && strings.get(i + 1).equals("TRUE")) {
                     answerList.add("TRUE");
                 } else {
                     answerList.add("FALSE");
@@ -380,7 +424,7 @@ public class Parser {
     }
 
     private boolean parseOr(ArrayList<String> strings) {
-        for (int i = 0;i<strings.size();i++) {
+        for (int i = 0; i < strings.size(); i++) {
             if (strings.get(i).equals("TRUE")) return true;
         }
         return false;
@@ -389,8 +433,8 @@ public class Parser {
     public void alter(String query, ArrayList<String> cNames, ArrayList<String> cTypes, ArrayList<HashMap<String, Object>> tableData) {
 
     }
-    /*public static void  main(String[] args) {
-        Main2 e = new Main2();
+    public static void  main(String[] args) {
+        Parser e = new Parser();
         ArrayList<HashMap<String,Object>> table =new ArrayList<HashMap<String,Object>>();
         HashMap<String, Object> row = new HashMap<String, Object>();
         row.put("subject", "Maths");
@@ -410,17 +454,17 @@ public class Parser {
         colTypes.add("varchar");
         colTypes.add("int");
         colTypes.add("varchar");
-        e.select("seLect * from table1 where age >= '5' and name = omar order by subject",colNames,colTypes,table);
+        //e.select("seLect * from table1 where age >= '5' and name = omar order by subject",colNames,colTypes,table);
         for (int i = 0; i < table.size(); i++) {
             System.out.println(table.get(i).get("name").toString() + " " + table.get(i).get("age").toString() + " " + table.get(i).get("subject").toString());
         }
-        e.update("UPDATE table_name SET subject = 'value1', age = '-2' WHERE not subject IN (Maths, 'Science') and not age >= '15' or not name = compu",colNames,colTypes,table);
+        e.update("UPDATE table_name SET subject = 'value1', age = -2 WHERE not subject IN ('Maths', 'Science') and not age >= 15 or not name = 'compu'",table,colNames,colTypes);
         System.out.println("\nAfter Update_1");
 
         for (int i = 0; i < table.size(); i++) {
             System.out.println(table.get(i).get("name").toString() + " " + table.get(i).get("age").toString() + " " + table.get(i).get("subject").toString());
         }
-        System.out.println("\nBefore Update_2");
+        /*System.out.println("\nBefore Update_2");
         for (int i = 0; i < table.size(); i++) {
             System.out.println(table.get(i).get("name").toString() + " " + table.get(i).get("age").toString() + " " + table.get(i).get("subject").toString());
         }
@@ -435,8 +479,8 @@ public class Parser {
         System.out.println(" \nAfter Delete");
         for (int i = 0; i < table.size(); i++) {
             System.out.println(table.get(i).get("name").toString() + " " + table.get(i).get("age").toString() + " " + table.get(i).get("subject").toString());
-        }
+        }*/
     }
 
-     */
+
 }
