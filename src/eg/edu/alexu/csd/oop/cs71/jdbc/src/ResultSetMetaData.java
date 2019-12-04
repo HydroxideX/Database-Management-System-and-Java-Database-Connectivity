1ad910b31fdd8f2f80b3a3eefa1205e42a490ebf
omarshawky15 <47141484+omarshawky15@users.noreply.github.com>
2019-12-05 01:50:02 +0200

    Copy_Paste

--- src/eg/edu/alexu/csd/oop/cs71/jdbc/src/ResultSetMetaData.java
+++ src/eg/edu/alexu/csd/oop/cs71/jdbc/src/ResultSetMetaData.java
@@ -18,39 +18,25 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData{
     }
 
     @Override
-    public boolean isAutoIncrement(int column) throws SQLException {
-        return false;
-    }
+    public boolean isAutoIncrement(int column) throws SQLException {		throw new UnsupportedOperationException();}
 
     @Override
-    public boolean isCaseSensitive(int column) throws SQLException {
-        return false;
-    }
+    public boolean isCaseSensitive(int column) throws SQLException {		throw new UnsupportedOperationException();}
 
     @Override
-    public boolean isSearchable(int column) throws SQLException {
-        return false;
-    }
+    public boolean isSearchable(int column) throws SQLException {		throw new UnsupportedOperationException();}
 
     @Override
-    public boolean isCurrency(int column) throws SQLException {
-        return false;
-    }
+    public boolean isCurrency(int column) throws SQLException {		throw new UnsupportedOperationException();}
 
     @Override
-    public int isNullable(int column) throws SQLException {
-        return 0;
-    }
+    public int isNullable(int column) throws SQLException {		throw new UnsupportedOperationException();}
 
     @Override
-    public boolean isSigned(int column) throws SQLException {
-        return false;
-    }
+    public boolean isSigned(int column) throws SQLException {		throw new UnsupportedOperationException();}
 
     @Override
-    public int getColumnDisplaySize(int column) throws SQLException {
-        return 0;
-    }
+    public int getColumnDisplaySize(int column) throws SQLException {		throw new UnsupportedOperationException();}
 
     @Override
     public String getColumnLabel(int column) throws SQLException {
@@ -63,19 +49,13 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData{
     }
 
     @Override
-    public String getSchemaName(int column) throws SQLException {
-        return null;
-    }
+    public String getSchemaName(int column) throws SQLException {		throw new UnsupportedOperationException();}
 
     @Override
-    public int getPrecision(int column) throws SQLException {
-        return 0;
-    }
+    public int getPrecision(int column) throws SQLException {		throw new UnsupportedOperationException();}
 
     @Override
-    public int getScale(int column) throws SQLException {
-        return 0;
-    }
+    public int getScale(int column) throws SQLException {		throw new UnsupportedOperationException();}
 
     @Override
     public String getTableName(int column) throws SQLException {
@@ -83,9 +63,7 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData{
     }
 
     @Override
-    public String getCatalogName(int column) throws SQLException {
-        return null;
-    }
+    public String getCatalogName(int column) throws SQLException {		throw new UnsupportedOperationException();}
 
     @Override
     public int getColumnType(int column) throws SQLException {
@@ -99,32 +77,20 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData{
     }
 
     @Override
-    public boolean isReadOnly(int column) throws SQLException {
-        return false;
-    }
+    public boolean isReadOnly(int column) throws SQLException {		throw new UnsupportedOperationException();}
 
     @Override
-    public boolean isWritable(int column) throws SQLException {
-        return false;
-    }
+    public boolean isWritable(int column) throws SQLException {		throw new UnsupportedOperationException();}
 
     @Override
-    public boolean isDefinitelyWritable(int column) throws SQLException {
-        return false;
-    }
+    public boolean isDefinitelyWritable(int column) throws SQLException {		throw new UnsupportedOperationException();}
 
     @Override
-    public String getColumnClassName(int column) throws SQLException {
-        return null;
-    }
+    public String getColumnClassName(int column) throws SQLException {		throw new UnsupportedOperationException();}
 
     @Override
-    public <T> T unwrap(Class<T> iface) throws SQLException {
-        return null;
-    }
+    public <T> T unwrap(Class<T> iface) throws SQLException {		throw new UnsupportedOperationException();}
 
     @Override
-    public boolean isWrapperFor(Class<?> iface) throws SQLException {
-        return false;
-    }
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {		throw new UnsupportedOperationException();}
 }
