45012f6283e09847afed0d4938ef3f1185c8c8f0
omarshawky15 <47141484+omarshawky15@users.noreply.github.com>
2019-12-07 00:34:34 +0200

    Update TimeoutBlock.java

--- src/eg/edu/alexu/csd/oop/cs71/jdbc/src/TimeoutBlock.java
+++ src/eg/edu/alexu/csd/oop/cs71/jdbc/src/TimeoutBlock.java
@@ -1,14 +1,71 @@
 package eg.edu.alexu.csd.oop.cs71.jdbc.src;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.sql.SQLException;
+import java.sql.Time;
+import java.util.concurrent.*;
+
 public class TimeoutBlock {
-    private final long timeoutMilliSeconds;
+    Future<Object> future ;
+    ExecutorService executor;
+    public Object addTask(Callable c , long time) throws SQLException {
+        Object x ;
+        executor = Executors.newSingleThreadExecutor();
+        future = executor.submit(c);
+        try {
+            x= (int)future.get(time, TimeUnit.SECONDS); //timeout is in 2 seconds
+        } catch (TimeoutException e) {
+            future.cancel(true);
+            throw new SQLException();
+        } catch (InterruptedException e) {
+            future.cancel(true);
+            throw new SQLException();
+        }
+        catch (ExecutionException e) {
+            future.cancel(true);
+            throw new SQLException();
+        }
+        executor.shutdownNow();
+        return  x;
+
+        /*if (future.isCancelled()){System.out.println("sorry");return false;}
+        else {System.out.println("good");
+            return true;}*/
+
+    }
+
+    public static void main(String[] args) throws SQLException {
+        TimeoutBlock t = new TimeoutBlock();
+        System.out.println((int) t.addTask(new Callable() {
+
+            public Object call() throws Exception {
+                int a = 0;
+                for (int i = 0; i < 10000000; i++) {a++;}
+                System.out.print("finished 1\n");
+                return a;
+            }
+        },500));
+
+        System.out.println((int)t.addTask(new Callable() {
+
+            public Object call() throws Exception {
+                int a = 0;
+                for (int i = 0; i < 5000000; i++) {a++;}
+                System.out.print("finished 2\n");
+                return a;
+            }
+        },500));
+
+    }
+    /*private final long timeoutMilliSeconds;
     private long timeoutInteval=100;
 
     public TimeoutBlock(long timeoutMilliSeconds){
         this.timeoutMilliSeconds=timeoutMilliSeconds;
     }
 
-    public void addBlock(Runnable runnable) throws Throwable{
+    public void addBlock(Callable runnable) throws Throwable{
         long collectIntervals=0;
         Thread timeoutWorker=new Thread(runnable);
         timeoutWorker.start();
@@ -24,17 +81,13 @@ public class TimeoutBlock {
         System.out.println("<<<<<<<<<<####>>>>>>>>>>> Timeout Block Executed Within "+collectIntervals+" Milli Seconds.");
     }
 
-    /**
-     * @return the timeoutInteval
-     */
+
     public long getTimeoutInteval() {
         return timeoutInteval;
     }
 
-    /**
-     * @param timeoutInteval the timeoutInteval to set
-     */
+
     public void setTimeoutInteval(long timeoutInteval) {
         this.timeoutInteval = timeoutInteval;
-    }
+    }*/
 }
